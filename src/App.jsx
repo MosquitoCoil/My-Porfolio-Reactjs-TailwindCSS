5ce093ff9efa68cc4e13f4f88a3b1f2c67f8ffe2
Jaymar Roco <131925937+MosquitoCoil@users.noreply.github.com>
2025-09-07 22:27:47 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from "./components/Header";
 import Main from "./pages/Main";
 import "./index.css";
 import Footer from "./components/Footer";
-import ProjectCarousel from "./pages/tryCarousel";
 
 function App() {
   return (
